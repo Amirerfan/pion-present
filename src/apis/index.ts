d6094fc94308bdd829ad3019bb3344cadc0b2762
Amirerfan Siamaki <amirerfan.siamaki@gmail.com>
2023-12-21 13:18:28 +0100

    Change ChatGPT Url

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create();
 
 export async function getChatGPTResponseAPI(message: string) {
-	const response = await axiosInstance.get(('http://18.221.100.91:8011/v1/?app=chatGPT&method=isTrue&params[question]=' + message), {
+	const response = await axiosInstance.get(('https://explorer.muon.net/alice/query/v1/?app=chatGPT&method=isTrue&params[question]=' + message), {
 			headers: {
 				'Content-Type': 'application/json',
 			}
